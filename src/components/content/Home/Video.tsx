eb4860a4324c4a37f659148ab090dffe9df030f5
unknown <raul@network-levelup.com>
2019-10-21 14:33:29 +0200

    video

--- src/components/content/Home/Video.tsx
+++ src/components/content/Home/Video.tsx
@@ -15,31 +15,31 @@ const MyVideoPlayer: React.FC = () => {
           "Fullscreen",
           "Captions"
         ]}
-        poster="./img/poster.jpg"
+        poster="/img/poster.jpg"
         // onCanPlayThrough={() => {
         //   // Do stuff
         // }}
       >
-        <source src="/img/levelUP-Video.mp4" type="video/mp4" />
+        <source src="/video/levelUP-Video.mp4" type="video/mp4" />
         <track
           label="Spanish"
           kind="subtitles"
           srcLang="es"
-          src="/img/videoSubtitles_es.vtt"
+          src="/video/videoSubtitles_es.vtt"
           default
         />
         <track
           label="English"
           kind="subtitles"
           srcLang="en"
-          src="/img/videoSubtitles_en.vtt"
+          src="/video/videoSubtitles_en.vtt"
           default
         />
         <track
           label="German"
           kind="subtitles"
           srcLang="ge"
-          src="/img/videoSubtitles_ge.vtt"
+          src="/video/videoSubtitles_ge.vtt"
           default
         />
       </Video>
